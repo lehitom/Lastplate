d6550124d512536893f64267d7ca0f2064c754fe
lehitom <48111403+lehitom@users.noreply.github.com>
2020-03-15 22:48:56 -0600

    Update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cool = require('cool-ascii-faces')
 const PORT = process.env.PORT || 5000
+const path = require('path')
 
 
 app.use(express.static(path.join(__dirname, 'public')))
