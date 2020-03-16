cdee465030eb7345eadde61e8e55bc170d5353b4
lehitom <48111403+lehitom@users.noreply.github.com>
2020-03-15 22:45:42 -0600

    Update

--- server.js
+++ server.js
@@ -5,5 +5,6 @@ const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/cool', (req, res) => res.send(cool()))
+app.get('/form', (req, res) => res.sendFile(path.join(__dirname+'/public/form.html')))
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
