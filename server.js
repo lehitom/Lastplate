b51222d3c16319d531b187d84083dede6268bacd
lehitom <48111403+lehitom@users.noreply.github.com>
2020-03-15 22:33:35 -0600

    Update

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+/*
 //require("dotenv").config();
 const express = require("express");
 // const bodyParser = require("body-parser");
@@ -19,8 +27,8 @@ app.get("/getChildren", getChildren);
 ​
 // app.use(express.static(path.join(__dirname, "public")));
 // app.set("views", path.join(__dirname, "views"));
-//app.set("view engine", "ejs");
-app.get('/', (req, res) => res.send('Hello World!'))
+//app.set("view engine", "ejs"); */
+//app.get('/', (req, res) => res.send('Hello World!'))
 //app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 ​/*
