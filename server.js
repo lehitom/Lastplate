ba57c8b2917cf22a5baa759556345e19cb3ee3fd
lehitom <48111403+lehitom@users.noreply.github.com>
2020-03-15 22:42:31 -0600

    Update

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const cool = require('cool-ascii-faces')
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/cool', (req, res) => res.send(cool()))
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
