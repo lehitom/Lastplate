89cb97d108290d3238daa72f77fd59c1bf1a66d7
lehitom <48111403+lehitom@users.noreply.github.com>
2020-03-15 22:47:30 -0600

    Update

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express()
 const cool = require('cool-ascii-faces')
 const PORT = process.env.PORT || 5000
 
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/cool', (req, res) => res.send(cool()))
 app.get('/form', (req, res) => res.sendFile(path.join(__dirname+'/public/form.html')))
