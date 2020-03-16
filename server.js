33359b7fe6142347d1d84f59fed71b816a2843ea
lehitom <48111403+lehitom@users.noreply.github.com>
2020-03-15 22:41:00 -0600

    Update

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
