d9fb02d4a31265b2a8b776e6eaa40e27368a2756
lehitom <48111403+lehitom@users.noreply.github.com>
2020-03-16 21:18:35 -0600

    Update

--- server.js
+++ server.js
@@ -41,10 +41,10 @@ function searchArea(res, zip) {
     if (err) {
       console.log(`Error in query: ${err}`);
     }
-	let buffer = resp.rows[0];
-	id = buffer.location_id;
+	let buffer = resp.rows[0].location_id;
+	// id = buffer.location_id;
     // response = resp.rows[0];
-	//id = JSON.stringify(test2);
+	id = JSON.stringify(buffer);
 	console.log(`${id}`);
   });
   
