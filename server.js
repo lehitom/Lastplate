0014180f713b08a54de9c8217aae23082a25c32f
lehitom <48111403+lehitom@users.noreply.github.com>
2020-03-16 21:09:02 -0600

    Update

--- server.js
+++ server.js
@@ -31,8 +31,9 @@ function findDeals(req, res) {
 
 function searchArea(res, zip) {
 	//const sql = "SELECT location_id FROM locations l WHERE l.zipcode != $1";
-	const sql = "SELECT * FROM locations";
+	//const sql = "SELECT * FROM locations";
 	//zip = sql; 
+  const sql = "SELECT table_name FROM information_schema.tables;"
   
   const values = [zip];
   let id = "";
