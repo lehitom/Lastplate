8670e99deebfc883bb524c644d9ac9d1e43b1759
lehitom <48111403+lehitom@users.noreply.github.com>
2020-03-16 20:51:38 -0600

    Update

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ function findDeals(req, res) {
 }
 
 function searchArea(res, zip) {
-	const sql = "SELECT location_id FROM locations l WHERE zipcode = $zip";
+	const sql = "SELECT location_id FROM locations l WHERE zipcode = 83221";
 	//zip = sql; 
   
   const values = [zip];
