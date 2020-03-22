77f67d5b70d3f0bc919c9dc2ebac495473d1df7c
lehitom <48111403+lehitom@users.noreply.github.com>
2020-03-21 18:45:59 -0600

    Update

--- public/homepage.js
+++ public/homepage.js
@@ -3,10 +3,10 @@ function search() {
 	console.log('Testing hardcoded DB');
 	
 	
-	$.get('https://infinite-brook-67283.herokuapp.com/find2?zip=83221', function(data, status) {
+	//$.get('https://infinite-brook-67283.herokuapp.com/find2?zip=83221', function(data, status) {
 		console.log('pinged URL');
 		console.log(status);
 		console.log(data);
-	});
+	//});
 	
 }
