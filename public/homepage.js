8d15a273f3d7b0737ba950c2c8d507d137bea75f
lehitom <48111403+lehitom@users.noreply.github.com>
2020-03-21 19:28:56 -0600

    Update

--- public/homepage.js
+++ public/homepage.js
@@ -3,7 +3,7 @@ function search() {
 	console.log('Testing hardcoded DB');
 	
 	
-	$.get('http://www.omdbapi.com/?i=tt3896198&apikey=4d452043', function(data, status) {
+	$.get('https://www.omdbapi.com/?i=tt3896198&apikey=4d452043', function(data, status) {
 		console.log('pinged URL');
 		console.log(status);
 		console.log(data);
