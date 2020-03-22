8aa97fda5235a8ca956771e916cbca10ac4b84e4
lehitom <48111403+lehitom@users.noreply.github.com>
2020-03-21 20:58:59 -0600

    Update

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ function searchArea2(res, zip) {
 	
 	console.log(test);
 	console.log(testing);
-	console.log(result);
+	console.log(testing[0].res_name);
 	//let res_list = 
 	//id = buffer.location_id;
     // response = resp.rows[0];
