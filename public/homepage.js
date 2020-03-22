ca7bce53357417dec129f2d4f4b8c76f766e06f2
lehitom <48111403+lehitom@users.noreply.github.com>
2020-03-21 19:27:31 -0600

    Update

--- public/homepage.js
+++ public/homepage.js
@@ -3,7 +3,7 @@ function search() {
 	console.log('Testing hardcoded DB');
 	
 	
-	$.get('https://infinite-brook-67283.herokuapp.com/find2?zip=83221', function(data, status) {
+	$.get('http://www.omdbapi.com/?i=tt3896198&apikey=4d452043', function(data, status) {
 		console.log('pinged URL');
 		console.log(status);
 		console.log(data);
