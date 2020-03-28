99a4fc9d22a73821399935e0f2d9beb46947e447
lehitom <48111403+lehitom@users.noreply.github.com>
2020-03-28 12:16:35 -0600

    Update

--- server.js
+++ server.js
@@ -4,16 +4,20 @@ const cool = require('cool-ascii-faces')
 const PORT = process.env.PORT || 5000
 const path = require('path')
 const bodyParser = require("body-parser")
+const session = require("express-session")
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(session({ secret: "secret word" }));
+app.use(logRequest);
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/public/test.html')))
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
+//app.get('/', (req, res) => res.send('Hello World!'))
 //app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/public/form2.html')))
 //app.get('/form', (req, res) => res.sendFile(path.join(__dirname+'/public/form.html')))
 //app.get('/getRestaurants', (req, res) => res.sendFile(path.join(__dirname+'/public/getRestaurants.php')))
