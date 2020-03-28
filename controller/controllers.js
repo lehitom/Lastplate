a09dddb20d057a2194bdff5d154f92512947e8e8
lehitom <48111403+lehitom@users.noreply.github.com>
2020-03-28 17:01:09 -0600

    Update

--- controller/controllers.js
+++ controller/controllers.js
@@ -4,7 +4,7 @@ const model = require("../model/models");
 exports.login = (req, res) => {
   console.log("USER: " + req.body.username + " PASSWORD: " + req.body.password);
   
-  model.checkCred = (req.body, (error, results) => {
+  model.checkCred(req.body, (error, results) => {
 	  if (error) {
 		  console.log("Error in login: " + error);
 	  } else {
