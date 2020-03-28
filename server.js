8a6bf89df8c90336e7489b00749d69b992d2c84d
lehitom <48111403+lehitom@users.noreply.github.com>
2020-03-28 12:07:45 -0600

    Update

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const path = require('path')
 const bodyParser = require("body-parser")
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
@@ -13,11 +15,10 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.send('Hello World!'))
 
 //app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/public/form2.html')))
-app.get('/form', (req, res) => res.sendFile(path.join(__dirname+'/public/form.html')))
+//app.get('/form', (req, res) => res.sendFile(path.join(__dirname+'/public/form.html')))
 //app.get('/getRestaurants', (req, res) => res.sendFile(path.join(__dirname+'/public/getRestaurants.php')))
-app.get('/find', findDeals)
-app.get('/find2', findDeals2)
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+//app.get('/find', findDeals)
+//app.get('/find2', findDeals2)
+
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
