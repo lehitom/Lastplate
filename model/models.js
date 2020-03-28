e74967b376327c6f6cd7d381734b01596e0cbb0a
lehitom <48111403+lehitom@users.noreply.github.com>
2020-03-28 14:03:38 -0600

    Update

--- model/models.js
+++ model/models.js
@@ -5,12 +5,13 @@ const pool = new Pool({ connectionString: connectionString })
 
 
 exports.checkCred = (params, callback) => {
+	console.log("here I am");
   let query = {
 	  text:
 		"SELECT username, password FROM credentials WHERE username=$1 AND password=$2",
 	  values: [params.user, params.password]
   };
-  
+  console.log("here I am");
   pool.query(query, (error, results) => {
 	 console.log("here I am");
     if (error || results.rows.length == 0) {
