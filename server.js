7e01cba605b0cd13f7a0201197f8aa57e5696803
lehitom <48111403+lehitom@users.noreply.github.com>
2020-03-28 19:48:27 -0600

    Update

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ let controllers = require("./controller/controllers");
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/public/login.html')))
 app.post('/login', controllers.login);
-//app.post('/isLoggedIn', controllers.isLoggedIn);
+app.post('/isLoggedIn', controllers.isLoggedIn);
 
 app.get('/form', (req, res) => res.render(path.join(__dirname+'/public/form2')));
 
