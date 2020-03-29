840fcbfaf5ae9c2187470a1d27802f9652a9490c
lehitom <48111403+lehitom@users.noreply.github.com>
2020-03-28 18:48:34 -0600

    Update

--- server.js
+++ server.js
@@ -20,9 +20,7 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/public/login.html'
 app.post('/login', controllers.login);
 //app.post('/isLoggedIn', controllers.isLoggedIn);
 
-app.get('/form', function(req, res) {
-		res.render(path.join(__dirname+'/public/form2.html'))
-});
+app.get('/form', (req, res) => res.sendFile(path.join(__dirname+'/public/form2.html')));
 
 //app.get('/', (req, res) => res.send('Hello World!'))
 //app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/public/form2.html')))
