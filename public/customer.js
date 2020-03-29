bccdd87ff6b0e1c7d2a6e3fd615f36f0a553f42e
lehitom <48111403+lehitom@users.noreply.github.com>
2020-03-28 19:59:28 -0600

    Update

--- public/customer.js
+++ public/customer.js
@@ -1,3 +1,18 @@
+function validZip() {
+	isVerified();
+	$.post("/login", params, function(result) {
+		if (result && result.success) {
+			$(".side").text("Successfully logged in.");
+			//$.get('/form2');
+			window.location.pathname = '/form';		
+		} else {
+			$(".side").text("Error logging in.");
+		}
+	});
+}
+
+
+
 function isVerified() {
 	$.post("/isLoggedIn", function(res) {
 				if (!(res && res.success)) {
