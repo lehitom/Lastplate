0a43080aa366ee853ff0fdd562414a4900b3addc
lehitom <48111403+lehitom@users.noreply.github.com>
2020-03-28 20:35:25 -0600

    Update

--- public/customer.js
+++ public/customer.js
@@ -4,11 +4,26 @@ function validZips() {
 	try {
 		let response = fetch("/validZips", {method:"POST"});
 		const json = response.json();
-		$(".side").text(json);
+		document.getElementById('side').appendChild(pushZips(json));
+		//pushZips(json);
 	}
 	catch (err){
 		console.log("error loading in zips")
 	}
+}
+
+function pushZips(json) {
+	
+	var list = document.createElement('ul);
+	
+	for (var i = 0; i < json.rowCount; i++) {
+		var item = document.createElement('li');
+		item.appendChild(document.createTextNode(json.row[i].zipcode));
+		list.appendChild(item);
+	}
+	return list;
+	
+	//json.forEach(row => {
 }
 
 function isVerified() {
