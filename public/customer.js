195d15358ab56ffc654b1b2f8adfa2106407f9b4
lehitom <48111403+lehitom@users.noreply.github.com>
2020-04-04 13:33:41 -0600

    messing with resturant loading

--- public/customer.js
+++ public/customer.js
@@ -46,6 +46,41 @@ function searchAreas() {
 	$.post("/searchAreas", params, function(result) {
 		if (result) {
 			$("#txtHint").empty();
+			var header = document.createElement('h3');
+			header.appendChild(document.createTextNode("Resturants in " + zipcode + ":");
+			$("#txtHint").append(header);
+			$("#txtHint").append(pushArea(result));
+		} else {
+			$("#txtHint").text("Error getting reply");
+		}
+	});
+}
+
+function pushArea(json) {
+	
+	
+	var list = document.createElement('ul');
+	
+	json.forEach(row => {
+		var item = document.createElement('li');
+		item.appendChild(document.createTextNode("ID: " + row.res_id + " is '" + row.res_name + "'"));
+		list.appendChild(item);
+	});
+	console.log(list);
+	return (list);
+
+}
+
+function searchRestaurants() {
+	var zipcode = $("#input").val();
+
+	var params = {
+		zipcode: zipcode
+	};
+
+	$.post("/searchAreas", params, function(result) {
+		if (result) {
+			$("#txtHint").empty();
 			var list = pushArea(result)
 			$("#txtHint").append(list);
 		} else {
